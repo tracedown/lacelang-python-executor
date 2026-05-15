0299201ef43bcada18d90a4a172ba703ae14f150
Max <neko@nekosamurai.cc>
2026-05-15 11:38:52 +0200

    0.9.2 specs - silences fix, emitRecovery (#8)

--- src/lacelang_executor/__init__.py
+++ src/lacelang_executor/__init__.py
@@ -1,6 +1,6 @@
 """Reference Python executor for the Lace probe scripting language."""
 
-__version__ = "0.1.0"
-__ast_version__ = "0.9.1"
+__version__ = "0.1.1"
+__ast_version__ = "0.9.2"
 
 from lacelang_executor.api import LaceExecutor, LaceProbe, LaceExtension  # noqa: F401, E402
